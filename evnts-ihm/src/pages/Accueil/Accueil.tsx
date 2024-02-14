9b1d41b758a611c948c310c9450e38f1f9c4de52
Caron Baptiste <baptiste.caron@etu.univ-orleans.fr>
2024-02-14 14:18:27 +0100

    :construction: cards event

--- evnts-ihm/src/pages/Accueil/Accueil.tsx
+++ evnts-ihm/src/pages/Accueil/Accueil.tsx
@@ -1,12 +1,35 @@
 import Navbar from '../../components/Navbar/Navbar';
 import './Accueil.scss';
-
+import CardEvent from '../../components/Card-Event/CardEvent';
 function Accueil() {
     return (
         <div className="pageAccueil">
-
-            <Navbar/>
+            <div className='asideNavbar'>
+                <Navbar/>
+            </div>
+        
             
+            <div className="allEventsContainer">
+               
+                    <CardEvent/>
+                    <CardEvent/>
+                    <CardEvent/>
+                    <CardEvent/>
+                    <CardEvent/>
+                    <CardEvent/> 
+                    <CardEvent/> 
+                    <CardEvent/> 
+                    <CardEvent/> 
+                    <CardEvent/> 
+                    <CardEvent/> 
+                    <CardEvent/> 
+                    <CardEvent/> 
+                    <CardEvent/> 
+                    <CardEvent/> 
+             
+                
+            </div>
+
         </div>
     );
 }
