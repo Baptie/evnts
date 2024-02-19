eb2392694fcc23897edbe77d15c097b521e5d5af
Aubert Vincent <vincent.aubert1@etu.univ-orleans.fr>
2024-02-19 22:55:48 +0100

    ✨ Initialisation des tables avec des inserts pour commencer les manipulations. ✨ Un fichier de création de table pour chaque module.

--- authentification-init.sql
+++ authentification-init.sql
@@ -1,17 +1,12 @@
 USE authentification;
 
-CREATE TABLE toto (
-                      id INT AUTO_INCREMENT PRIMARY KEY,
-                      name VARCHAR(255) NOT NULL
+CREATE TABLE Utilisateur (
+                      idUtilisateur INT AUTO_INCREMENT PRIMARY KEY,
+                      email VARCHAR(255) NOT NULL,
+                      pseudo VARCHAR(255) NOT NULL,
+                      motDePasse VARCHAR(255) NOT NULL
 );
-INSERT INTO toto (name) VALUES ('Authentification Entry');
+INSERT INTO Utilisateur (email,pseudo, motDePasse) VALUES ('vincent.aubert@evnt.com', 'vince','123456');
 
 
-USE Salon;
-
-CREATE TABLE toto (
-                      id INT AUTO_INCREMENT PRIMARY KEY,
-                      name VARCHAR(255) NOT NULL
-);
-INSERT INTO toto (name) VALUES ('Salon Entry');
 
--- utilisateur-init.sql
+++ utilisateur-init.sql
@@ -1,7 +1,30 @@
 USE utilisateur;
 
-CREATE TABLE toto (
-                      id INT AUTO_INCREMENT PRIMARY KEY,
-                      name VARCHAR(255) NOT NULL
+CREATE TABLE User (
+                      idUser INT AUTO_INCREMENT PRIMARY KEY,
+                      email VARCHAR(255) NOT NULL,
+                      pseudo VARCHAR(255) NOT NULL,
+                      description TEXT,
+                      status VARCHAR(50),
+                      photo VARCHAR(255)
 );
-INSERT INTO toto (name) VALUES ('Utilisateur Entry');
+
+CREATE TABLE Message (
+                         idMessage INT AUTO_INCREMENT PRIMARY KEY,
+                         contenu TEXT NOT NULL,
+                         dateEnvoi DATETIME NOT NULL,
+                         idSender INT,
+                         idReceiver INT,
+                         FOREIGN KEY (idSender) REFERENCES User(idUser),
+                         FOREIGN KEY (idReceiver) REFERENCES User(idUser)
+);
+
+
+-- Créer les utilisateurs Vince et Sid
+INSERT INTO User (email, pseudo, description, status, photo) VALUES ('vince@bg.com', 'Vince', 'Description de Vince', 'Actif', 'nerd.png');
+INSERT INTO User (email, pseudo, description, status, photo) VALUES ('sid@gital.com', 'Sid', 'Description de Sid', 'Actif', 'chauve.png');
+
+-- Ajouter deux messages entre Vince et Sid
+INSERT INTO Message (contenu, dateEnvoi, idSender, idReceiver) VALUES ('Salut Sid, comment ça va ?', NOW(), 1, 2);
+INSERT INTO Message (contenu, dateEnvoi, idSender, idReceiver) VALUES ('Salut Vince, ça va bien, merci !', NOW(), 2, 1);
+
